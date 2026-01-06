3728d786a8f48d8d296020b7a165cc4db2da03b0
Miguel Cobián Meraz <miguelcobian99@gmail.com>
2026-01-06 12:32:14 -0600

    Feat. Mejora la página de Pokemons: se añade navegación entre páginas y se actualiza el título dinámicamente.

--- src/app/pages/pokemons/pokemons-page.ts
+++ src/app/pages/pokemons/pokemons-page.ts
@@ -11,6 +11,10 @@ import { PokemonsList } from '../../pokemons/components/pokemons-list/pokemons-l
 import { PokemonListSkeleton } from './ui/pokemon-list-skeleton/pokemon-list-skeleton';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, tap } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemons-page',
@@ -19,24 +23,51 @@ import { SimplePokemon } from '../../pokemons/interfaces';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class PokemonsPage implements OnInit {
-  isLoading = signal(true);
   private appRef = inject(ApplicationRef);
-
   private pokemonsService = inject(PokemonsService);
+
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private title = inject(Title);
+
+  currentPage = toSignal(
+    this.route.queryParamMap.pipe(
+      map((params) => params.get('page') ?? '1'),
+      map((page) => (isNaN(+page) ? 1 : +page)),
+      map((page) => Math.max(1, page))
+    )
+  );
+
   pokemons = signal<SimplePokemon[]>([]);
+
+  // isLoading = signal(true);
 
   private $appState = this.appRef.isStable.subscribe((isStable) => {
     console.log({ isStable });
   });
 
   public loadPokemons(nextPage = 0) {
-    this.pokemonsService.loadPage(nextPage).subscribe((pokemons) => {
-      console.log('ON INIT');
-      this.pokemons.set(pokemons);
-    });
+    const pageToLoad = this.currentPage()! + nextPage;
+    this.pokemonsService
+      .loadPage(pageToLoad)
+      .pipe(
+        tap(() =>
+          this.router.navigate([], {
+            queryParams: {
+              page: pageToLoad,
+            },
+          })
+        ),
+        tap(() => this.title.setTitle(`Pokemons SSR - Page ${pageToLoad}`))
+      )
+      .subscribe((pokemons) => {
+        console.log('ON INIT');
+        this.pokemons.set(pokemons);
+      });
   }
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe(console.log);
     this.loadPokemons();
   }
 
