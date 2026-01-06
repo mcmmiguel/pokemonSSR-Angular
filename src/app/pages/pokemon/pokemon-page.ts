cb1b220edb7d392d6b6a256449270da14ba8f6a6
Miguel Cobián Meraz <miguelcobian99@gmail.com>
2026-01-06 15:47:55 -0600

    Feat. Actualizar la página de Pokémon: se añade gestión de metadatos y título dinámico al cargar información del Pokémon.

--- src/app/pages/pokemon/pokemon-page.ts
+++ src/app/pages/pokemon/pokemon-page.ts
@@ -8,6 +8,8 @@ import {
 import { Pokemon } from '../../pokemons/interfaces';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemon-page',
@@ -18,6 +20,8 @@ import { ActivatedRoute } from '@angular/router';
 export default class PokemonPage implements OnInit {
   pokemonsService = inject(PokemonsService);
   private route = inject(ActivatedRoute);
+  private title = inject(Title);
+  private meta = inject(Meta);
 
   pokemon = signal<Pokemon | null>(null);
 
@@ -25,6 +29,33 @@ export default class PokemonPage implements OnInit {
     const id = this.route.snapshot.paramMap.get('id') || '';
     if (!id) return;
 
-    this.pokemonsService.loadPokemon(id).subscribe(this.pokemon.set);
+    this.pokemonsService
+      .loadPokemon(id)
+      .pipe(
+        tap(({ id, name }) => {
+          const pageTitle = `#${id} - ${name}`;
+          const pageDescription = `#${id} - ${name}`;
+
+          this.title.setTitle(pageTitle);
+
+          this.meta.updateTag({
+            name: 'description',
+            content: 'Información sobre el pokémon ' + name,
+          });
+          this.meta.updateTag({
+            name: 'og:title',
+            content: 'Información sobre el pokémon ' + name,
+          });
+          this.meta.updateTag({
+            name: 'og:description',
+            content: pageDescription,
+          });
+          this.meta.updateTag({
+            name: 'og:image',
+            content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+          });
+        })
+      )
+      .subscribe(this.pokemon.set);
   }
 }
