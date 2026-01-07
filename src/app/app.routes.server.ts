5f42c9662a44567afe07e635ad11fdab144d5e97
Miguel Cobián Meraz <miguelcobian99@gmail.com>
2026-01-07 17:38:14 -0600

    Fix. Reorganizar rutas del servidor para mejorar la legibilidad

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
-  {
     path: 'pokemons/:id',
     renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
   },
 ];
